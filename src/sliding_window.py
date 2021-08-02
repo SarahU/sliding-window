806aa339e79eb139728473b6916f541a6acc4a90
Sarah Usher <sarahnusher@gmail.com>
2021-08-02 20:52:09 +0100

    brute force max total completed - green

--- src/sliding_window.py
+++ src/sliding_window.py
@@ -1,2 +1,19 @@
-def sliding_window_brute_force_max_sum():
-    return 0
+def sliding_window_brute_force_max_sum(arr, k):
+    max: int = len(arr)
+    max_total: int = 0
+
+    for i in range(0, max):
+        if i+k < max:
+            total = 0
+            for j in range(0, k):
+                total += arr[i+j]
+            if max_total < total:
+                max_total = total
+
+    return max_total
+
+# def sliding_window_brute_force_all_totals():
+#     return 0
+#
+# input = [12, 20, 36, 4, 18, 8]
+#     k = 3
--- tests/test_sliding_window.py
+++ tests/test_sliding_window.py
@@ -1,10 +1,21 @@
 import pytest
-from src.sliding_window import sliding_window_brute_force_max_sum
+from src.sliding_window import sliding_window_brute_force_max_sum \
+    # , sliding_window_brute_force_all_totals
 
-def test_sliding_window_brute_force():
+
+def test_sliding_window_brute_force_max_sum():
     input = [12, 20, 36, 4, 18, 8]
     k = 3
-    actualResult = sliding_window_brute_force_max_sum()
+    actualResult = sliding_window_brute_force_max_sum(input, k)
 
-    expectedResult = 1
+    expectedResult = 68
     assert expectedResult == actualResult
+
+
+# def test_sliding_window_brute_force_max_sum():
+#     input = [12, 20, 36, 4, 18, 8]
+#     k = 3
+#     actualResult = sliding_window_brute_force_all_totals()
+#
+#     expectedResult = [68, 60, 58]
+#     assert expectedResult == actualResult
